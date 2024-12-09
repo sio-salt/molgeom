b563157b5842ac585c52cae022187bc4abd81026
sio-salt <saltinthedesertyou@gmail.com>
2024-12-09 12:49:45 +0900

    remove unnecessary comments

--- interfaces/poscar2xyz.py
+++ interfaces/poscar2xyz.py
@@ -11,12 +11,10 @@ python poscar2xyz.py <poscar_file_path> a_ini a_fin b_ini b_fin c_ini c_fin
 a_ini, a_fin, b_ini, b_fin, c_ini, c_fin: range of cell repetitions. accepts only integers.
 e.g. `python poscar2xyz.py POSCAR -1 2 -1 2 -1 2` will generate 3x3x3 supercell with the original cell at the center.
 """
-    # rep_cell_a: list[int] = [0, 1],
-    # rep_cell_b: list[int] = [0, 1],
-    # rep_cell_c: list[int] = [0, 1],
+
 
 def main():
-    if len(sys.argv) < 8:
+    if len(sys.argv) != 8:
         print(help_msg)
         sys.exit(1)
 
@@ -35,9 +33,9 @@ def main():
 
     mole = poscar_parser(filepath)
     mole.replicate(
-        [cell_rep[0], cell_rep[1]],
-        [cell_rep[2], cell_rep[3]],
-        [cell_rep[4], cell_rep[5]]
+        cell_rep[0:2],
+        cell_rep[2:4],
+        cell_rep[4:],
     )
     print(mole.to_xyz())
 
