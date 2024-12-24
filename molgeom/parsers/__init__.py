5402240b930f5fea61c0fe69f2e514e0856f216f
sio-salt <saltinthedesertyou@gmail.com>
2024-12-24 13:47:32 +0900

    add gaussian input head tail extractor

--- molgeom/parsers/__init__.py
+++ molgeom/parsers/__init__.py
@@ -1,6 +1,6 @@
 from .cif import cif_parser
 from .gamess import gms_inp_parser
-from .gaussian import gau_inp_parser
+from .gaussian import gau_inp_parser, gau_inp_head_tail
 from .parser_selector import read_file
 from .poscar import poscar_parser
 from .xyz import xyz_parser
@@ -8,6 +8,7 @@ from .xyz import xyz_parser
 __all__ = [
     "xyz_parser",
     "gau_inp_parser",
+    "gau_inp_head_tail",
     "gms_inp_parser",
     "cif_parser",
     "poscar_parser",
