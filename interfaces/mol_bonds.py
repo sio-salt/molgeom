13f1bcf708c5684b642c18b1f2a375ed24bf65ca
sio-salt <saltinthedesertyou@gmail.com>
2025-01-06 19:35:14 +0900

    mol_bonds handle one file

--- interfaces/mol_bonds.py
+++ interfaces/mol_bonds.py
@@ -9,14 +9,14 @@ if importlib.util.find_spec("readline"):
 
 
 def main():
-    if len(sys.argv) < 2:
-        print("Usage: python script.py <file_path1> <file_path2> ...")
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <file_path>")
         sys.exit(1)
     else:
-        for filepath in sys.argv[1:]:
-            if not os.path.isfile(filepath):
-                print(f"File not found: {filepath}")
-                sys.exit(1)
+        filepath = sys.argv[1]
+        if not os.path.isfile(filepath):
+            print(f"File not found: {filepath}")
+            sys.exit(1)
 
     print()
     while True:
@@ -37,27 +37,26 @@ def main():
         # upper_bound = float(input("Enter the upper bond threshold: "))
 
     print()
-    filepaths = sys.argv[1:]
-    for filepath in filepaths:
-        print(filepath)
-        output = []
-        mole = read_file(filepath)
-        bonds = mole.get_bonds(tolerance)
-        output.append(f"Number of bonds: {len(bonds)}")
-        output.append(f"bond tolerance: {tolerance}")
+    filepath = sys.argv[1]
+    print(filepath)
+    output = []
+    mole = read_file(filepath)
+    bonds = mole.get_bonds(tolerance)
+    output.append(f"Number of bonds: {len(bonds)}")
+    output.append(f"bond tolerance: {tolerance}")
+    output.append(
+        "label   bond length (Angstrom)       "
+        + "atom1                                                                          -     atom2"
+    )
+    for label, bond_dict in enumerate(bonds):
+        i, j = bond_dict["pair"]
+        ai, aj = mole[(i, j)]
+        dist_angst = ai.distance_to(aj)
         output.append(
-            "label   bond length (Angstrom)       "
-            + "atom1                                                                          -     atom2"
+            f"{label:3d}          {dist_angst:.9f}           {i+1:3d}   {ai}    -   {j+1:3d}   {aj}"
         )
-        for label, bond_dict in enumerate(bonds):
-            i, j = bond_dict["pair"]
-            ai, aj = mole[(i, j)]
-            dist_angst = ai.distance_to(aj)
-            output.append(
-                f"{label:3d}          {dist_angst:.9f}           {i+1:3d}   {ai}    -   {j+1:3d}   {aj}"
-            )
-        print("\n".join(output))
-        print()
+    print("\n".join(output))
+    print()
 
     # output_file = "bonds_output.txt"
     # with open(output_file, "w") as f:
